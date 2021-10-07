0007063a63662e622a4c1260dc5d116eb3b561e0
Facinorous <63425706+Facinorous-420@users.noreply.github.com>
2021-10-06 20:25:13 -0400

    Reduce code of random number function (#3)
    Reduced the code I did with my last commit
    +Fix my mistakes, cause no one reviewed my code ;)

--- src/index.ts
+++ src/index.ts
@@ -21,13 +21,10 @@ export const execAsync = async (cmd: string, opts: shelljs.ExecOptions = {}): Pr
 
 /**
  * Generates a random number between 1 and specified max
- * @param min OPTIONAL: lowest possible number, defaults to 0
  * @param max highest possible number
+ * @param min OPTIONAL: lowest possible number, defaults to 0
  */
-export const randomNumber = (min?:number, max: number) => {
-  !min ? min = 0 : null
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
+export const randomNumber = (max: number, min: number = 0) => Math.floor(Math.random() * (max - min + 1) + min)
 
 /**
  * ForEach method made asynchronous
